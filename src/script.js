007afbe0377e36a5b5ac5d2daccdc6185fb54ed3
swapnilsharm881 <swapnilsharma881@gmail.com>
2025-03-30 14:02:57 +0530

    Learinng about texture

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { cameraNormalMatrix } from "three/tsl";
+import { cameraNormalMatrix, texture } from "three/tsl";
 import { Pane } from "tweakpane";
 const canvas = document.querySelector("canvas")
 
@@ -9,57 +9,95 @@ const pane = new Pane();
 
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("#222"); // 
+// scene.background = new THREE.Color("#000000"); // 
+const axesHelper = new THREE.AxesHelper( 4 );
+scene.add( axesHelper );
+
+// Texture Loader
+const textureLoader = new THREE.TextureLoader();
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  150,
   window.innerWidth / window.innerHeight,
   0.1,
   200
 );
-camera.position.z = 10;
+camera.position.z = 5;
+camera.position.y = 10;
 
 
 // Light
-// const light = new THREE.AmbientLight(0xffffff, 0.1);
-// scene.add(light);
+const light = new THREE.AmbientLight(0xffffff, 10);
+scene.add(light);
 
-const pointLight = new THREE.PointLight(0xffffff, 5);
-pointLight.position.set(0, 5, 10);
+const pointLight = new THREE.PointLight(0xffffff, 2);
+pointLight.position.set(5, 0, 0);
 scene.add(pointLight);
 const pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
 scene.add(pointLightHelper);
 
-
 // Geometry
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16);
-const planeGeometry = new THREE.PlaneGeometry(1, 1);
-const icosahedronGeometry = new THREE.IcosahedronGeometry(3, 0);
+const planeGeometry = new THREE.PlaneGeometry(100, 100);
+const sphereGeometry = new THREE.SphereGeometry(.5,32,32);
+const cylinderGeometry = new THREE.CylinderGeometry(0.5,0.5,1,32);
 
 // Material
-const color = "#F2EFE7"
-const basicMaterial = new THREE.MeshBasicMaterial({ color: color }); // ✅ Set color in material
-const standardMaterial = new THREE.MeshStandardMaterial({color : color})
-const physicalMaterial = new THREE.MeshPhysicalMaterial({color : color})
-const phongMaterial = new THREE.MeshPhongMaterial({ color: color }); // ✅ Set color in material
-const lambertMatterial = new THREE.MeshLambertMaterial({ color: color }); // ✅ Set color in material
+const basicMaterial = new THREE.MeshBasicMaterial({  }); // ✅ Set color in material
+const standardMaterial = new THREE.MeshStandardMaterial({})
+const physicalMaterial = new THREE.MeshPhysicalMaterial({})
+const phongMaterial = new THREE.MeshPhongMaterial({  }); // ✅ Set color in material
+const lambertMatterial = new THREE.MeshLambertMaterial({  }); // ✅ Set color in material
 
+// Texture
+const testexture = textureLoader.load("/textures/space-cruiser-panels2-bl/space-cruiser-panels2_albedo.png");
+testexture.repeat.set(2,2)
+
+testexture.wrapS = THREE.MirroredRepeatWrapping
+testexture.wrapT = THREE.MirroredRepeatWrapping
+
+testexture.wrapS = THREE.RepeatWrapping
+testexture.wrapT = THREE.RepeatWrapping
+
+basicMaterial.map = testexture
 
 // Meshes
-const geometry = [boxGeometry,torusKnotGeometry]
-const materials = [basicMaterial,standardMaterial]
-const meshes  = materials.map((material, index) => {
-  const mesh = new THREE.Mesh(geometry[1],material);
+const geometries = [boxGeometry,planeGeometry,cylinderGeometry,torusKnotGeometry,sphereGeometry]
+const materials = [basicMaterial]
+const meshes  = geometries.map((geometry, index) => {
+  const mesh = new THREE.Mesh(geometry,materials[0]);
   mesh.position.set(
-    Math.random() * 10 - 4, // X between -5 and 5
-    Math.random() * 10 - 4, // Y between -5 and 5
-    Math.random() * 10 - 5  // Z between -5 and 5
+    0,0,0
   );
-  scene.add(mesh)
   return mesh
 })
+
+
+//Change Mesh Props
+const cube = meshes[0]
+const plane = meshes[1]
+const cylinder = meshes[2]
+const torus = meshes[3]
+const sphere = meshes[4]
+
+// plane.position.x = 1.5;
+cylinder.position.y = 1.5;
+torus.position.y = -1.5;
+sphere.position.x = -1.5;
+// plane.rotation.x = Math.PI
+
+//Adding Meshes
+const group = new THREE.Group()
+group.add(plane)
+
+scene.add(group)
+
+
+
+
+
 
 
 
